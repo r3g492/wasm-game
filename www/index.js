6980fca08e67e1e71dad90643604066c6b870871
r3g492 <57958064+r3g492@users.noreply.github.com>
2022-05-08 22:00:10 +0900

    220508 updates ..

--- www/index.js
+++ www/index.js
@@ -1,29 +1,60 @@
-async function init() {
+import init, {World} from "wasm-game";
 
-    const memory = new WebAssembly.Memory({initial: 1});
+/*
+async function start() {
+    const wasm = await init();
+    greet("myName");
+    console.log("no probs");
+}
+*/
 
-    const importObject = {
-        js: {
-            mem: memory
-        },
-        console: {
-            log: () => {
-                console.log("Just logging something...");
-            },
-            error: () => {
-                console.log("error!");
-            }
+
+init().then(_ => {
+
+    const world = World.new();
+
+    const CELL_SIZE = 10;
+    const worldWidth = world.width();
+
+    console.log(world.width());
+    const canvas = document.getElementById("wasm-canvas");
+    const ctx = canvas.getContext("2d");
+
+    canvas.height = worldWidth * CELL_SIZE;
+    canvas.width = worldWidth * CELL_SIZE;
+
+    function drawWorld() {
+        ctx.beginPath();
+        for (let x = 0; x < worldWidth + 1; x++) {
+            ctx.moveTo(CELL_SIZE * x, 0);
+            ctx.lineTo(CELL_SIZE * x, worldWidth * CELL_SIZE);
+
         }
+        for (let y = 0; y <worldWidth + 1; y++) {
+            ctx.moveTo(0, CELL_SIZE * y);
+            ctx.lineTo(worldWidth * CELL_SIZE, CELL_SIZE * y, );
+        }
+
+
+        ctx.stroke();
     }
 
-    const response = await fetch("sum.wasm");
-    const buffer = await response.arrayBuffer();
-    debugger
-    await WebAssembly.instantiate(buffer, importObject);
-    debugger
+    function drawSnake() {
+        const snakeIdx = world.snake_head_idx();
+        const col = snakeIdx % worldWidth;
+        const row = Math.floor(snakeIdx / worldWidth);
 
-    const uint8Array = new Uint8Array(memory.buffer, 0, 2);
-    const hiText = new TextDecoder().decode(uint8Array);
-    console.log(hiText);
-}
-init();
+        ctx.beginPath();
+        ctx.fillRect(
+            col * CELL_SIZE,
+            row * CELL_SIZE,
+            CELL_SIZE,
+            CELL_SIZE
+        );
+        ctx.stroke();
+    }
+
+
+    drawWorld();
+    drawSnake();
+})
