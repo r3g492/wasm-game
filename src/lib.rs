4b00d7890d35f13d01ba64aa928d0b9406444ae8
r3g492 <57958064+r3g492@users.noreply.github.com>
2022-05-09 22:58:05 +0900

    220509 updates ..

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
 use wasm_bindgen::prelude::*;
+use std::rc::Rc;
+use wasm_bindgen::JsCast;
+use std::fmt;
 
 struct SnakeCell(usize);
 
@@ -34,6 +37,11 @@ impl World {
     pub fn snake_head_idx(&self) -> usize {
         self.snake.body[0].0
     }
+    pub fn update(&mut self) {
+        let snake_idx = self.snake_head_idx();
+        self.snake.body[0].0 = snake_idx + 1;
+    }
 }
+
 
 // wasm-pack build --target web
--- www/index.js
+++ www/index.js
@@ -1,19 +1,19 @@
 import init, {World} from "wasm-game";
-
-/*
-async function start() {
-    const wasm = await init();
-    greet("myName");
-    console.log("no probs");
-}
-*/
-
+import Universe from "wasm-game";
 
 init().then(_ => {
+    const GRID_COLOR = "#5490a9";
+    const DEAD_COLOR = "#f80000";
+    const ALIVE_COLOR = "#ffffff";
+
+// Construct the universe, and get its width and height.
+    /*const universe = Universe.new();
+    const width = universe.width();
+    const height = universe.height();*/
 
     const world = World.new();
 
-    const CELL_SIZE = 10;
+    const CELL_SIZE = 50;
     const worldWidth = world.width();
 
     console.log(world.width());
@@ -23,8 +23,43 @@ init().then(_ => {
     canvas.height = worldWidth * CELL_SIZE;
     canvas.width = worldWidth * CELL_SIZE;
 
+    /*document.addEventListener("keydown", (e) => {
+        console.log(e.code)
+    })
+    document.addEventListener("mousemove", mousemove)
+
+    function mousemove(event){
+        console.log("pageX: ",event.pageX,
+            "pageY: ", event.pageY,)
+    }*/
+
+    /*document.addEventListener("click", function() {
+        alert("Hello World!");
+    });*/
+
+    /*canvas.addEventListener("mousedown", event => {
+        const boundingRect = canvas.getBoundingClientRect();
+
+        const scaleX = canvas.width / boundingRect.width;
+        const scaleY = canvas.height / boundingRect.height;
+
+        const canvasLeft = (event.clientX - boundingRect.left) * scaleX;
+        const canvasTop = (event.clientY - boundingRect.top) * scaleY;
+
+        const row = Math.min(Math.floor(canvasTop / (CELL_SIZE + 1)), height - 1);
+        const col = Math.min(Math.floor(canvasLeft / (CELL_SIZE + 1)), width - 1);
+
+        universe.toggle_cell(row, col);
+
+        drawCells();
+        drawGrid();
+    });*/
+
+
+
     function drawWorld() {
         ctx.beginPath();
+        ctx.strokeStyle = GRID_COLOR;
         for (let x = 0; x < worldWidth + 1; x++) {
             ctx.moveTo(CELL_SIZE * x, 0);
             ctx.lineTo(CELL_SIZE * x, worldWidth * CELL_SIZE);
@@ -45,6 +80,7 @@ init().then(_ => {
         const row = Math.floor(snakeIdx / worldWidth);
 
         ctx.beginPath();
+        ctx.fillStyle = DEAD_COLOR;
         ctx.fillRect(
             col * CELL_SIZE,
             row * CELL_SIZE,
@@ -57,4 +93,12 @@ init().then(_ => {
 
     drawWorld();
     drawSnake();
+
+    /*setInterval(() => {
+        console.log("Update 100ms")
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        drawWorld();
+        drawSnake();
+        world.update();
+    }, 100)*/
 })
